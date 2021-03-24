9eff53e996dd4df05621a10731c2e8a82c0e2124
husnijabir <husnijabir@gmail.com>
2021-03-24 17:33:26 +0530

    Added related files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropDown = () => setDropdownOpen(prev => !prev);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let users = await axios.get("/user/all");
+      setUsers(users.data);
+    }
+
+    fetchData();
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Dropdown isOpen={dropdownOpen} toggle={toggleDropDown}>
+          <DropdownToggle caret>
+            Users
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem header>Header</DropdownItem>
+            <DropdownItem>Some Action</DropdownItem>
+            <DropdownItem text>Dropdown Item Text</DropdownItem>
+            <DropdownItem disabled>Action (disabled)</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>Foo Action</DropdownItem>
+            <DropdownItem>Bar Action</DropdownItem>
+            <DropdownItem>Quo Action</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </header>
     </div>
   );
