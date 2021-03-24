862abd4a4f19022b39437998f256fe08d25339cf
husnijabir <husnijabir@gmail.com>
2021-03-24 21:55:56 +0530

    Created the User Selection component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
-import { useEffect, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { ToastContainer } from 'react-toastify';
+import UserSelection from './components/UserSelection';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggleDropDown = () => setDropdownOpen(prev => !prev);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      let users = await axios.get("/user/all");
-      setUsers(users.data);
-    }
-
-    fetchData();
-  }, [])
-
-
   return (
     <div className="App">
       <header className="App-header">
+        <ToastContainer />
         <img src={logo} className="App-logo" alt="logo" />
-        <Dropdown isOpen={dropdownOpen} toggle={toggleDropDown}>
-          <DropdownToggle caret>
-            Users
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem header>Header</DropdownItem>
-            <DropdownItem>Some Action</DropdownItem>
-            <DropdownItem text>Dropdown Item Text</DropdownItem>
-            <DropdownItem disabled>Action (disabled)</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>Foo Action</DropdownItem>
-            <DropdownItem>Bar Action</DropdownItem>
-            <DropdownItem>Quo Action</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+        <UserSelection />
       </header>
     </div>
   );
---
+++ src/components/UserSelection.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Spinner } from 'reactstrap';
+import { toast } from 'react-toastify';
+
+const UserSelection = () => {
+    const [users, setUsers] = useState([]);
+    const [userId, setUserId] = useState(null);
+    const [userName, setUserName] = useState('Select Users');
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleDropDown = () => setDropdownOpen(prev => !prev);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setIsLoading(true);
+                let users = await axios.get("/user/all");
+                setUsers(users.data);
+            } catch (error) {
+                toast.error(error?.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchData();
+    }, [])
+
+    const selectUser = (event) => {
+        console.log('selected', event.target);
+        setUserName(event.target.name);
+        setUserId(event.target.value);
+    }
+
+    return (
+        <div>
+            {isLoading ?
+                <Spinner color="primary" /> :
+                <div>
+                    {users.length > 0 && <Dropdown isOpen={dropdownOpen} toggle={toggleDropDown}>
+                        <DropdownToggle caret>
+                            {userName}
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            {
+                                users.map(user => {
+                                    return (<DropdownItem value={user.userId} name={user.name} onClick={selectUser}>{user.name}</DropdownItem>);
+                                })
+                            }
+                        </DropdownMenu>
+                    </Dropdown>}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default UserSelection;
